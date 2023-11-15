34217bf34df17616424d643de609d2a794073026
Guilherme Costa <guilherme.partic@gmail.com>
2023-11-15 14:58:31 -0300

    update: created the models

--- work/models.py
+++ work/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models import Field
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Work(models.Model):
+    title: Field = models.CharField(max_length=255)
+    slug: models.Field = models.SlugField(unique=True)
+    description: Field = models.TextField(max_length=1250)
+    link: Field = models.URLField(max_length=255)
+    cover: Field = models.ImageField(upload_to='works/covers/')
+    created_at: models.Field = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+
+class WorkImage(models.Model):
+    work: models.Field = models.ForeignKey(Work, on_delete=models.CASCADE)
+    image: models.Field = models.ImageField(
+        upload_to='works/images/',  # type: ignore
+        )
+
+    def __str__(self) -> str:
+        return f'image_id {self.pk}'
+
+
+# criar função para redimensionar as imagens
