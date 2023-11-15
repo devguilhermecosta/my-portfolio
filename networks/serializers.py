2188dcab38e0353beb9f31525a9d10e6e70b22d3
Guilherme Costa <guilherme.partic@gmail.com>
2023-11-15 00:19:57 -0300

    update: created the validate method

--- networks/serializers.py
+++ networks/serializers.py
@@ -1,5 +1,6 @@
 from . models import Networks
 from rest_framework import serializers
+from collections import defaultdict
 
 
 class NetworksSerializer(serializers.ModelSerializer):
@@ -13,3 +14,17 @@ class NetworksSerializer(serializers.ModelSerializer):
             'phone',
             'email',
         ]
+
+    def validate(self, attrs):
+        _errors = defaultdict(list)
+
+        for field, value in attrs.items():
+            if not value or value == '':
+                _errors[field].append(
+                    'Campo obrigatório',
+                )
+
+        if _errors:
+            raise serializers.ValidationError(_errors)
+
+        return super().validate(attrs)
