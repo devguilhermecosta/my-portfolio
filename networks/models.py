3f781bbab5e88cbc80cadb0591ca8c5a24a52e14
Guilherme Costa <guilherme.partic@gmail.com>
2023-11-15 00:19:21 -0300

    update: removed the error_messages, blank and null parameters

--- networks/models.py
+++ networks/models.py
@@ -2,39 +2,10 @@ from django.db import models
 from django.db.models import Field
 
 
-error_messages = {
-    'required': 'este campo é obrigatório',
-}
-
-
 class Networks(models.Model):
-    instagram: Field = models.CharField(max_length=255,
-                                        blank=False,
-                                        null=False,
-                                        error_messages=error_messages,
-                                        )
-    linkedin: Field = models.CharField(max_length=255,
-                                       blank=False,
-                                       null=False,
-                                       error_messages=error_messages,
-                                       )
-    github: Field = models.CharField(max_length=255,
-                                     blank=False,
-                                     null=False,
-                                     error_messages=error_messages,
-                                     )
-    whatsapp: Field = models.CharField(max_length=255,
-                                       blank=False,
-                                       null=False,
-                                       error_messages=error_messages,
-                                       )
-    phone: Field = models.CharField(max_length=255,
-                                    blank=False,
-                                    null=False,
-                                    error_messages=error_messages,
-                                    )
-    email: Field = models.EmailField(max_length=255,
-                                     blank=False,
-                                     null=False,
-                                     error_messages=error_messages,
-                                     )
+    instagram: Field = models.CharField(max_length=255, blank=True)
+    linkedin: Field = models.CharField(max_length=255, blank=True)
+    github: Field = models.CharField(max_length=255, blank=True)
+    whatsapp: Field = models.CharField(max_length=255, blank=True)
+    phone: Field = models.CharField(max_length=255, blank=True)
+    email: Field = models.EmailField(max_length=255, blank=True)
