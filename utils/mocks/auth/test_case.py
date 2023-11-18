f36eef70b2b99ecbc885df4c8677ed7ee3485aba
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-11-18 14:22:14 -0300

    update: add a tearDown method

--- utils/mocks/auth/test_case.py
+++ utils/mocks/auth/test_case.py
@@ -1,9 +1,21 @@
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+from django.test import override_settings
 from .. users.mocks import make_superuser
+import contextlib
+import shutil
 
 
+TEST_DIR = 'test_data'
+
+
+@override_settings(MEDIA_ROOT=(TEST_DIR + '/media'))
 class APITestCaseWithLogin(APITestCase):
+    def tearDown(self) -> None:
+        with contextlib.suppress(OSError):
+            shutil.rmtree(TEST_DIR)
+        return super().tearDown()
+
     def make_login(self) -> tuple[User, str]:
         # create a user
         user = make_superuser()
