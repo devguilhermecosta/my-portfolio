a0d3a89424bfa2ae69e6987ccd4de075b978c052
Guilherme Costa <guilherme.partic@gmail.com>
2023-11-19 17:36:45 -0300

    update: add the CSRF_TRUSTED_ORIGINS

--- project/settings/security.py
+++ project/settings/security.py
@@ -13,6 +13,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = string_to_list('ALLOWED_HOSTS')
 
+CSRF_TRUSTED_ORIGINS = string_to_list('CSRF_TRUSTED_ORIGINS')
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
